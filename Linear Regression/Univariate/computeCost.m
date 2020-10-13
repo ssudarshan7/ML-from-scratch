46c576f5cc9844a4d3f6c31f56d98c7b5ddf717b
Sathyanarayan Sudarshan <ssudarshan7@gatech.edu>
2020-10-13 01:15:03 -0400

    Update computeCost.m

--- Linear Regression/Univariate/computeCost.m
+++ Linear Regression/Univariate/computeCost.m
@@ -9,9 +9,6 @@ m = length(y); % number of training examples
 % You need to return the following variables correctly 
 J = 0;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta
-%               You should set J to the cost.
 
 
 J = (1/(2*m))*sum((X*theta - y).^2);
