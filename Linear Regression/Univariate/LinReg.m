3e3bd3c405d664a04cfa55e4ee5e51d807d3c910
Sathyanarayan Sudarshan <ssudarshan7@gatech.edu>
2020-10-13 01:12:40 -0400

    Update and rename ex1.m to LinReg.m

--- Linear Regression/Univariate/LinReg.m
+++ Linear Regression/Univariate/LinReg.m
@@ -1,27 +1,4 @@
-%% Machine Learning Online Class - Exercise 1: Linear Regression
 
-%  Instructions
-%  ------------
-%
-%  This file contains code that helps you get started on the
-%  linear exercise. You will need to complete the following functions
-%  in this exericse:
-%
-%     warmUpExercise.m - Done
-%     plotData.m - Done
-%     gradientDescent.m
-%     computeCost.m - Done
-%     gradientDescentMulti.m
-%     computeCostMulti.m
-%     featureNormalize.m
-%     normalEqn.m
-%
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
-% x refers to the population size in 10,000s
-% y refers to the profit in $10,000s
-%
 
 %% Initialization
 clear ; close all; clc
